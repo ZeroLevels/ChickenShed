36d2987ab3fc422d76ffbf63d62f8f74700f5563
GrygrFlzr <grygrflzr@hotmail.com>
2014-03-18 21:56:34 +0800

    ForgeGradle update part 2

--- src/main/java/vazkii/chickenshed/ChickenShed.java
+++ src/main/java/vazkii/chickenshed/ChickenShed.java
@@ -19,7 +19,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 @Mod(
 		modid = "ChickenShed",
 		name = "Chicken Shed",
-		version = "1.1.3"
+		version = "1.1.4"
 	)
 public class ChickenShed {
 
