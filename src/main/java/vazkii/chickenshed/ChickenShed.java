65d20c167bdc87ff7c1c2d48ac17197a1ceee4e8
Zero <ZeroLevels@yahoo.com>
2014-11-26 18:15:16 -0500

    Fixed version.
    For real this time.  I forgot to say that adding new features resets the final number.  New feature versioning works kind of like how new Minecraft versioning.  The second number changes when new features are added. Is that too complicated? >.>  Oh well.

--- src/main/java/vazkii/chickenshed/ChickenShed.java
+++ src/main/java/vazkii/chickenshed/ChickenShed.java
@@ -27,7 +27,7 @@ import vazkii.chickenshed.handler.ConfigurationHandler;
 public class ChickenShed {
 	public static final String MODID   = "ChickenShed";
 	public static final String NAME    = "Chicken Shed";
-	public static final String VERSION = "1.2.4";
+	public static final String VERSION = "1.2.0";
 	
 	public static Configuration config;
 
